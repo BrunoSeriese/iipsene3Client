3166b5abdd16ed8acc70a2511a5f3b3df83c102d
VinSe <vincentmcmaps@gmail.com>
2021-11-24 10:20:23 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './content/question/question.component';
 import { AnswerListComponent } from './content/question/answer-list/answer-list.component';
-import { AnswerComponent } from './content/question/answer-list/answer/answer.component';
+import { AnswerComponent } from './content/shared/answer/answer.component';
 import { NextComponent } from './content/shared/navigation/next/next.component';
 import { PreviousComponent } from './content/shared/navigation/previous/previous.component';
 
