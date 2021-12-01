2570a3bdf7535e80d24d97eececcae9ddd896ae4
Bruno <bruno.pleysier@gmail.com>
2021-12-01 12:38:52 +0100

    added result options

--- src/app/content/result/result.component.ts
+++ src/app/content/result/result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Result} from "./result.model";
+import {Answer} from "../shared/answer/answer.model";
 
 @Component({
   selector: 'app-result',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+
+  result:Result = new Result(3, "je krijgt subsidie B", new Answer(1, "je hebt verloren", 4))
 
   constructor() { }
 
