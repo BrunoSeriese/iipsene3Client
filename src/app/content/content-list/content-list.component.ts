b7bb825d434fc177743a402fb61727648dce28cd
Bruno <bruno.pleysier@gmail.com>
2021-12-01 11:03:11 +0100

    added content-list

---
+++ src/app/content/content-list/content-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.css']
+})
+export class ContentListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
