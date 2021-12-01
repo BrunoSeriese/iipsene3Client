cb1261760b3fd4bf9d422c185d8f3f25c18d2c05
VinSe <vincentmcmaps@gmail.com>
2021-12-01 11:40:25 +0100

    Added methods to get current, next and previous content

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Content} from "./content.interface";
+import {ContentListComponent} from "./content-list/content-list.component";
+
 
 @Component({
   selector: 'app-content',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  private contentId: number = 1;
+  private lastContentId: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public getContentId(): number {
+    return this.contentId;
+  }
+
+  public getNextContentId(id: number) {
+    this.lastContentId = this.contentId;
+    this.contentId = id;
+  }
+
+  public getLastContentId(): number {
+    return this.lastContentId;
+  }
+
+  public getContent(): Content {
+    return this.getContentById(this.contentId);
+  }
+
+  public getContentById(id: number): Content {
+    let contentList: Content[] = new ContentListComponent().contentList;
+    for(let content of contentList) {
+      if (content.getId() == id) {
+        return content;
+      }
+    }
+  }
+
+  public nextContent(): void {
+    let content: Content = this.getContentById(this.contentId);
+    //TODO: Set the Content
+  }
+
+  public previousContent(): void {
+    this.contentId = this.lastContentId;
+    let content: Content = this.getContent();
   }
 
 }
