4ab43390d11d1a2a92d4e409d924a15efc4d9bee
VinSe <vincentmcmaps@gmail.com>
2021-12-08 17:56:40 +0100

    Added DoCheck to update AnswerList

--- src/app/content/question/answer-list/answer-list.component.ts
+++ src/app/content/question/answer-list/answer-list.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {Answer} from "../../shared/answer/answer.model";
+import {ContentService} from "../../content.service";
 
 @Component({
   selector: 'app-answer-list',
   templateUrl: './answer-list.component.html',
   styleUrls: ['./answer-list.component.scss']
 })
-export class AnswerListComponent implements OnInit {
-  answerList: Answer[] = [
-    new Answer(1, 'Ja', 2),
-    new Answer(2, 'Nee', 2),
-    new Answer(3, 'Misschien', 3),
-    new Answer(4, 'Af en toe', 3)
-  ];
+export class AnswerListComponent implements OnInit, DoCheck {
+  answerList: Answer[] = this.contentService.getContent().getAnswer();
 
-
-  constructor() { }
+  constructor(private contentService: ContentService) { }
 
   ngOnInit(): void {
+  }
+
+  ngDoCheck() {
+    //TODO: Make this better
+    this.answerList = this.contentService.getContent().getAnswer();
+  }
+
+  public radioChangeHandler(event: any){
+    return this.contentService.radioChangeHandler(event)
   }
 
 }
