2c34bccd79017f35a57970290bec4e0f7f7a8b01
VinSe <vincentmcmaps@gmail.com>
2021-12-08 14:39:39 +0100

    changed the content dynamically in the html

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Content} from "./content.interface";
 import {ContentListComponent} from "./content-list/content-list.component";
 import {ContentService} from "./content.service";
+import {Question} from "./question/question.model";
+import {Result} from "./result/result.model";
 
 
 @Component({
@@ -10,6 +12,8 @@ import {ContentService} from "./content.service";
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
+  content: Content = new Question(99, "Test Question", null);
+  // content: Content = new Result(99, "Test Result", null);
 
   constructor(private contentService: ContentService) {
   }
@@ -19,6 +23,14 @@ export class ContentComponent implements OnInit {
 
   public getContent() {
     return this.contentService.getContent();
+  }
+
+  public getInstance(): String {
+    if(this.content instanceof Question) {
+      return "Question";
+    } else if (this.content instanceof Result) {
+      return "Result";
+    }
   }
 
 }
