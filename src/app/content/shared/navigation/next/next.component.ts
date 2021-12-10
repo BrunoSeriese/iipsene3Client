1644564193cedd11383163e8e063e5fc7f7720c9
VinSe <vincentmcmaps@gmail.com>
2021-12-10 11:57:27 +0100

    Renamed methods and/or variables

--- src/app/content/shared/navigation/next/next.component.ts
+++ src/app/content/shared/navigation/next/next.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ContentService} from "../../../content.service";
+import {NavigationService} from "../../navigation.service";
 
 
 @Component({
@@ -9,23 +10,21 @@ import {ContentService} from "../../../content.service";
 })
 export class NextComponent implements OnInit {
 
-  constructor(private contentService: ContentService) {
-  }
+  constructor(private contentService: ContentService,
+              private navigationService: NavigationService) { }
 
   public ngOnInit(): void {
   }
 
-  public onSelection(){
-    return this.contentService.getSelectedValue() == '';
+  public onNoAnswerSelected(): boolean {
+    return this.navigationService.isSelectedAnswerEmpty();
   }
 
-
   public onNextClick(): void {
-    if (this.contentService.getSelectedValue() != ''){
-      this.contentService.setNextContentId(Number(this.contentService.getSelectedValue()));
-      this.contentService.setSelectedValue();
+    if (!this.navigationService.isSelectedAnswerEmpty()){
+      this.contentService.setNextContentId(this.navigationService.getSelected());
+      this.navigationService.clearSelected();
     }
-
   }
 
 }
