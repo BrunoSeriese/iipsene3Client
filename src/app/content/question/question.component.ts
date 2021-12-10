b1a688c711910dc588d2145759ad9efb1dad8a85
VinSe <vincentmcmaps@gmail.com>
2021-12-10 11:58:06 +0100

    Catch @Input and/or @Output

--- src/app/content/question/question.component.ts
+++ src/app/content/question/question.component.ts
@@ -1,24 +1,18 @@
-import {Component, DoCheck, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Question} from "./question.model";
-import {Content} from "../content.interface";
-import {ContentService} from "../content.service";
+import {NavigationService} from "../shared/navigation.service";
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.scss']
 })
-export class QuestionComponent implements OnInit, DoCheck {
-  public question:Question = <Question> this.contentService.getContent();
+export class QuestionComponent implements OnInit {
+  @Input() public question: Question;
 
-  constructor(private contentService: ContentService) { }
+  constructor(public navigationService: NavigationService) { }
 
-  ngOnInit(): void {
-  }
-
-  ngDoCheck() {
-    //TODO: Make this better
-    this.question = <Question> this.contentService.getContent();
+  public ngOnInit(): void {
   }
 
 }
