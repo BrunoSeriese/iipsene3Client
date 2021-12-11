64acce0370c7fd600c23f3a0bfe2c6992ec99c29
VinSe <vincentmcmaps@gmail.com>
2021-12-11 16:18:33 +0100

    Removed ngOnInit and NavigationService

--- src/app/content/result/result.component.ts
+++ src/app/content/result/result.component.ts
@@ -1,19 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Result} from "./result.model";
-import {NavigationService} from "../shared/navigation.service";
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent {
   @Input() public result: Result;
 
-  constructor(private navigationService: NavigationService) { }
-
-  public ngOnInit(): void {
-    this.navigationService.setSelected(this.result.getAnswer()[0].getNextContentId());
-  }
+  constructor() { }
 
 }
