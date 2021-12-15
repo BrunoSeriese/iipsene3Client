cdfb3bf16a7c69ab567f5941bb42db1ae1b7bf90
VinSe <vincentmcmaps@gmail.com>
2021-12-15 11:42:30 +0100

    declaring question

--- src/app/content/content-component/question/question.component.spec.ts
+++ src/app/content/content-component/question/question.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuestionComponent } from './question.component';
+import {Question} from "./question.model";
+import {Answer} from "../../shared/answer/answer.model";
+import {AnswerListComponent} from "./answer-list/answer-list.component";
+import {ContentComponent} from "../../content.component";
+import {AppComponent} from "../../../app.component";
 
 describe('QuestionComponent', () => {
   let component: QuestionComponent;
@@ -8,7 +13,7 @@ describe('QuestionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QuestionComponent ]
+      declarations: [ QuestionComponent, AnswerListComponent ]
     })
     .compileComponents();
   });
@@ -16,6 +21,14 @@ describe('QuestionComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(QuestionComponent);
     component = fixture.componentInstance;
+
+    component.question = new Question(1, "Is koffie lekker?", [
+      new Answer(1, "Ja"),
+      new Answer(2, "Beetje wel"),
+      new Answer(3, "Beetje niet"),
+      new Answer(4, "Nee"),
+    ]);
+
     fixture.detectChanges();
   });
 
