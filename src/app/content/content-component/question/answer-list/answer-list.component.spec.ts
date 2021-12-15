74919bd26dfac804ebdad06256b7a40b93a0a548
Bruno <bruno.pleysier@gmail.com>
2021-12-15 12:09:49 +0100

    tests question.component.spec.ts

--- src/app/content/content-component/question/answer-list/answer-list.component.spec.ts
+++ src/app/content/content-component/question/answer-list/answer-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnswerListComponent } from './answer-list.component';
+import {EventEmitter} from "@angular/core";
 
 describe('AnswerListComponent', () => {
   let component: AnswerListComponent;
@@ -16,10 +17,18 @@ describe('AnswerListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AnswerListComponent);
     component = fixture.componentInstance;
+
+   component.selectedAnswerEvent.emit(5);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be a eventEmitter when called', () => {
+    expect(component.selectedAnswerEvent).toEqual(new EventEmitter<number>())
+  });
+
+
 });
