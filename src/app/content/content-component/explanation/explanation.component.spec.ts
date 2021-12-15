6c5ae773a7acf8a5e546203cee687b79d1dd2378
VinSe <vincentmcmaps@gmail.com>
2021-12-15 11:44:48 +0100

    declaring input variable

--- src/app/content/content-component/explanation/explanation.component.spec.ts
+++ src/app/content/content-component/explanation/explanation.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExplanationComponent } from './explanation.component';
+import {Explanation} from "./explanation.model";
+import {Answer} from "../../shared/answer/answer.model";
 
 describe('ExplanationComponent', () => {
   let component: ExplanationComponent;
@@ -16,6 +18,9 @@ describe('ExplanationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExplanationComponent);
     component = fixture.componentInstance;
+
+    component.explanation = new Explanation(6, "Je gaat een sterke kop koffie krijgen", new Answer(9, "Die heb je verdient!"));
+
     fixture.detectChanges();
   });
 
