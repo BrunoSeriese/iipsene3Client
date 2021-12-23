2ee35b2b1ad71e04f51036336bd7e0ad635e8117
BrunoSeriese <bruno.pleysier@gmail.com>
2021-12-24 00:12:59 +0200

    update 2 content.model.ts and DAO

--- src/app/content/content.DAO.ts
+++ src/app/content/content.DAO.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from "rxjs";
 import {Content} from "./content.interface";
+import {ContentModel} from "./content.model";
 
 
 @Injectable()
@@ -12,8 +13,12 @@ export class ContentDao {
   constructor(private http: HttpClient) {
     this.contentUrl = 'http://localhost:8080/api/v1/content';
   }
-  public findAll(): Observable<Content[]> {
-    return this.http.get<Content[]>(this.contentUrl);
+  public findAll(): Observable<ContentModel[]> {
+    return this.http.get<ContentModel[]>(this.contentUrl);
+  }
+  public getContent(){
+    // var contents[ContentModel] = this.findAll();
+
   }
 
 
