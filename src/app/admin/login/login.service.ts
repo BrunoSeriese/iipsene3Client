6b062477316763fb8e4a10c9f4af8926c5dc39bd
VinSe <vincentmcmaps@gmail.com>
2022-01-12 10:30:15 +0100

    Added login form

--- src/app/admin/login/login.service.ts
+++ src/app/admin/login/login.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private baseURL: String = "http://localhost:8080/api/v1";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public login(email: string, password: string): Observable<string> {
+    let body: unknown = {
+      "email": email,
+      "password": password
+    }
+    return this.http
+      .post(this.baseURL + "/auth/login", body, {responseType: "text"});
+  }
 }
