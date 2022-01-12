0fd78ac43c6b477a79be19eb40c6554cddbf907b
VinSe <vincentmcmaps@gmail.com>
2022-01-12 16:51:23 +0100

    DAO

--- src/app/content/content.DAO.ts
+++ src/app/content/content.DAO.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs";
-import {Content} from "./content.model";
+import {ContentModel} from "./content.model";
+import {Content} from "./content.interface";
+import {ContentService} from "./content.service";
+import {Question} from "./content-component/question/question.model";
+import {Video} from "./content-component/video/video.model";
+import {Result} from "./content-component/result/result.model";
+import {Explanation} from "./content-component/explanation/explanation.model";
+import {ContentTreeService} from "./tree/content-tree.service";
 
 
 @Injectable({
@@ -9,19 +16,42 @@ import {Content} from "./content.model";
 })
 export class ContentDAO {
   private baseURL: String = "http://localhost:8080/api/v1";
+  private contents: Content[];
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private contentTreeService: ContentTreeService) {
+    this.getAll().subscribe(contents => {
+      this.contents = contents;
+      this.contentTreeService.create(this.contents);
+    });
   }
 
-  public findAll(): Observable<Content[]> {
+  public getAll(): Observable<ContentModel[]> {
     return this.http
-      .get<Content[]>(this.baseURL + "/contents");
+      .get<ContentModel[]>(this.baseURL + "/contents");
   }
 
-  public getContent(){
-    // var contents[ContentModel] = this.findAll();
-
+  public createAll(contents: ContentModel[]) {
+    let newContents: Content[] = this.convertArray(contents);
   }
+
+  public convertArray(contents: ContentModel[]) {
+    let newContents: Content[] = [];
+    for (let content of contents) {
+      if(content.type == "Question") {
+        newContents.push(new Question(content.id, content.value, content.answers));
+      } else if(content.type == "Video") {
+        newContents.push(new Video(content.id, content.value, content.answers[0]));
+      } else if(content.type == "Result") {
+        newContents.push(new Result(content.id, content.value, content.answers[0]));
+      } else if(content.type == "Explanation") {
+        newContents.push(new Explanation(content.id, content.value, content.answers[0]));
+      }
+    }
+    return newContents;
+  }
+
+
 
 
 }
