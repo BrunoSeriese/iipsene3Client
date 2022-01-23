3509bed8a5692d12b6b1d9073802f8ecd19682b5
Shallow-RB <ryan.bhola2601@gmail.com>
2022-01-23 19:05:02 +0100

    half werkende dropdown voor content toevoegen

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ContentDAO} from "../../content/content.DAO";
 import {ContentModel} from "../../content/content.model";
 
@@ -9,6 +9,10 @@ import {ContentModel} from "../../content/content.model";
 })
 export class DashboardComponent implements OnInit {
   public contents: ContentModel[] = [];
+  public selectedContent: ContentModel;
+  public selectedContentType: String = "";
+  public newArray: any[] = [];
+
 
   constructor(private contentDAO: ContentDAO) {
     this.getContent();
@@ -17,12 +21,28 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public getContent(){
-    this.contentDAO.getAll().subscribe(contents =>{
+  public getContent() {
+    this.contentDAO.getAll().subscribe(contents => {
       this.contents = contents
     })
   }
 
 
+  public getUniqueTypes() {
+    for (const a of this.contents) {
+      if (!this.newArray.includes(a.type)) {
+        this.newArray.push(a.type)
+      }
+    }
+  }
 
+
+  public onSelected(content: ContentModel): void {
+    this.selectedContent = content;
+    this.selectedContentType = content.type;
+  }
+
+  test() {
+
+  }
 }
