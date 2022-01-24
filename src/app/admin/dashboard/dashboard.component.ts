492536c47f6b3529ac618b08e4579bc8bec0cb34
Shallow-RB <ryan.bhola2601@gmail.com>
2022-01-24 13:04:36 +0100

    working question toevoegen

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ContentDAO} from "../../content/content.DAO";
 import {ContentModel} from "../../content/content.model";
+import {Question} from "../../content/content-component/question/question.model";
+import {Answer} from "../../content/shared/answer/answer.model";
 
 @Component({
   selector: 'app-dashboard',
@@ -9,9 +11,10 @@ import {ContentModel} from "../../content/content.model";
 })
 export class DashboardComponent implements OnInit {
   public contents: ContentModel[] = [];
-  public selectedContent: ContentModel;
-  public selectedContentType: String = "";
-  public newArray: any[] = [];
+  public selectedContentType: string = "";
+  public uniqueTypeArray: string[] = ['Question', 'Explanation', 'Video', 'Result'];
+  private content: ContentModel;
+  private answerArray: Answer[] = [];
 
 
   constructor(private contentDAO: ContentDAO) {
@@ -21,28 +24,27 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
   public getContent() {
     this.contentDAO.getAll().subscribe(contents => {
       this.contents = contents
     })
   }
 
+  public getSelectedType(selectedType: string): void {
+    this.selectedContentType = selectedType;
 
-  public getUniqueTypes() {
-    for (const a of this.contents) {
-      if (!this.newArray.includes(a.type)) {
-        this.newArray.push(a.type)
-      }
-    }
+  }
+
+  getValues(question: string, answer1: Answer) {
+
+    this.answerArray = []
+    this.answerArray.push(answer1)
+    let newID = this.contents[this.contents.length-1].id + 1; //TODO kijken naar ID voor adding en removing
+    // console.log(newID);
+    this.contents.push(this.content = new ContentModel(newID, question, this.selectedContentType, this.answerArray))
+    console.log(this.contents)
   }
 
 
-  public onSelected(content: ContentModel): void {
-    this.selectedContent = content;
-    this.selectedContentType = content.type;
-  }
-
-  test() {
-
-  }
 }
