f2a88c94deae521d093b039c74cbaab49919e62e
VinSe <vincentmcmaps@gmail.com>
2022-01-25 15:09:33 +0100

    add content functions added (wrong ids for some reason)

--- src/app/admin/dashboard/dashboard-content/dashboard-content.component.ts
+++ src/app/admin/dashboard/dashboard-content/dashboard-content.component.ts
@@ -8,6 +8,7 @@ import {Explanation} from "../../../content/content-component/explanation/explan
 import {Video} from "../../../content/content-component/video/video.model";
 import {SharedNodeService} from "./shared-node.service";
 import {ContentService} from "../../../content/content.service";
+import {Answer} from "../../../content/shared/answer/answer.model";
 
 @Component({
   selector: 'app-dashboard-content',
@@ -17,6 +18,7 @@ import {ContentService} from "../../../content/content.service";
 export class DashboardContentComponent implements OnInit {
   @Input("nodes") public nodes: Node;
   @Input("node") public node: Node;
+  public
 
   constructor(private sharedNodeService: SharedNodeService,
               public contentService: ContentService) {
@@ -30,13 +32,66 @@ export class DashboardContentComponent implements OnInit {
     this.sharedNodeService.updateSelectedNode(this.node);
   }
 
+  public addNode() {
+    let answerIds: number[] = this.getAnswerIds(this.nodes, []);
+    let answerId: number = this.getLowestNonExistingId(answerIds, 0, answerIds.length - 1);
+    this.node.content.answers.push(new Answer(answerId, ""));
+
+    let contentIds: number[] = this.getContentIds(this.nodes, []);
+    let contentId: number= this.getLowestNonExistingId(contentIds, 0, contentIds.length - 1);
+    this.node.addChild(new Node(new Question(contentId, "", [])));
+  }
+
   public removeNode(): void {
     let parent: Node = this.contentService.getParentNodeByNode(this.node);
-    console.log(parent.content.value)
     let index: number = parent.getChildren().indexOf(this.node);
-    console.log(index);
     parent.content.answers.splice(index, 1);
     parent.removeChild(this.node);
+  }
+
+  public getLowestNonExistingId(list: number[], first: number, last: number) {
+    if(first > last) {
+      return last + 1;
+    }
+
+    if(first != list[first] - 1) {
+      return first;
+    }
+
+    let mid: number = (first + last) / 2;
+
+    if(list[mid] - 1 == mid) {
+      return this.getLowestNonExistingId(list, mid + 1, last);
+    }
+    return this.getLowestNonExistingId(list, first, mid);
+  }
+
+  public getContentIds(node: Node, contentIds: number[]): number[] {
+    if(node == null) {
+      return;
+    }
+
+    contentIds.push(node.content.id);
+
+    for(let child of node.getChildren()) {
+      this.getAnswerIds(child, contentIds);
+    }
+    return contentIds;
+  }
+
+  public getAnswerIds(node: Node, answerIds: number[]): number[] {
+    if(node == null) {
+      return;
+    }
+
+    node.content.answers.forEach(answer => {
+      answerIds.push(answer.id);
+    })
+
+    for(let child of node.getChildren()) {
+      this.getAnswerIds(child, answerIds);
+    }
+    return answerIds;
   }
 
   public getInstance(content: Content): string {
