ac62f3a1a63d2463a36ba3402042f335fa07253f
VinSe <vincentmcmaps@gmail.com>
2022-03-18 15:34:09 +0100

    Added to host on Heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ipsen3-app'));
+app.use(express.static('./dist/iipsene3Client'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/ipsen3-app/'}),
+  res.sendFile('index.html', {root: 'dist/iipsene3Client/'}),
 );
 
 // Start the app by listening on the default Heroku port
